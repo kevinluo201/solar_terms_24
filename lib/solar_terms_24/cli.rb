dae78078b6445acde64876cfdc05ce866caabb40
Kevin Luo <kevin.luo@telus.com>
2022-12-29 21:06:13 -0500

    add options

--- lib/solar_terms_24/cli.rb
+++ lib/solar_terms_24/cli.rb
@@ -9,9 +9,9 @@ module SolarTerms24
     option :lang, type: :string, default: 'en', desc: 'Language: en, zh-TW, or ja'
     option :timezone, type: :string, default: 'UTC', desc: 'Timezone: UTC, Asia/Taipei, Asia/Tokyo, etc.'
     def list(year)
-      solar_terms = SolarTerms.new(year)
+      solar_terms = SolarTerms.new(year, timezone: options[:timezone], lang: options[:lang])
       solar_terms.solar_terms.each_value do |solar_term|
-        puts "#{solar_term.name}: #{solar_term.datetime}"
+        puts "#{solar_term.name}: #{solar_term.datetime.strftime("%Y-%m-%d %H:%M")}"
       end
     end
   end
